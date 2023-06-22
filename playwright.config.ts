acea66ae747f1b66031c795aa676f27285d52e81
HoangDung2 <113884858+HoangDung2@users.noreply.github.com>
2023-06-22 15:53:54 +0700

    Jobs Login

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,41 @@ const config: PlaywrightTestConfig={
     outputFile: "jsonReport/jsonReport.json"
   }], ["html",{
         open:"never",
-  }]]
+  }]],
+  import { PlaywrightTestConfig, devices } from '@playwright/test';
+// import { json } from 'node:stream/consumers';
+
+const config: PlaywrightTestConfig={
+  testMatch:["tests/fileLogin.ts"],
+  use: {
+     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+     actionTimeout: 0,
+     /* Base URL to use in actions like `await page.goto('/')`. */
+     // baseURL: 'http://localhost:000',
+ /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    //  trace: 'on-first-retry',
+      baseURL:"https://opensource-demo.orangehrmlive.com/",
+      // baseURL:'http://localhost:3000',
+      headless:false,
+      video:"on",
+      screenshot:"on",
+      steps:"on",
+      launchOptions: {
+      },
+      trace: "on-first-retry"
+  },
+  retries:0,
+  reporter: [["dot"], ["json",{
+    outputFile: "jsonReport/jsonReport.json"
+  }], ["html",{
+        open:"never",
+  }]],
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+
+  ]
 };
 export default config;
